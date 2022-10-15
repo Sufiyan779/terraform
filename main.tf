f8ad1744c3fef64feeacb06836bc527d67291fa4
sufiyan <khansufiyan779@gmail.com>
2022-10-15 15:08:55 +0530

    Added new branch1

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
 resource "aws_s3_bucket"{
-    bucket = "sadaf"
+    bucket   = "sadaf"
+    tags     =
+    {
+        
+       Name  = "subrat1"
+    }
 }
 
 resource "aws_vpc_" "my_vpc"{
-    cidr_block = "192.168.0.0/16"
-    name       = "terrafrom"
+    cidr_block     = "192.168.0.0/16"
+    tags           =
+    {
+        
+        Name      = "terrafrom"
+    }    
 }
